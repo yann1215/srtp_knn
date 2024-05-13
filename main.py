d58ddd3a7a09844a6e2f93c2f047d2c88ac9b289
Yann <yanshen0117@outlook.com>
2024-05-13 16:59:59 +0800

    2024/5/13 16:59 laptop

---
+++ main.py
@@ -0,0 +1,11 @@
+import sklearn
+import processData
+
+
+def knn():
+    resultData = 0
+    print("resultData = ", resultData)
+
+# Main
+if __name__ == '__main__':
+    knn()
---
+++ processData.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def calc_character_data(inData):
+    outData = inData
+    return outData
+
+
+rawData = np.load()
+
+labelData = np.array()
+
+characterData = calc_character_data(rawData)
