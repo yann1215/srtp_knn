7b80f691d5f8555f4298fd40e2f47fc91ae2e492
Yann <yanshen0117@outlook.com>
2024-05-14 21:13:51 +0800

    2024/5/14 21:13 master

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import pandas as pd
 import sklearn
-import processData
+from sklearn.neighbors import KNeighborsClassifier
 
+import processData  # run processData.py
 
-def knn():
-    resultData = 0
-    print("resultData = ", resultData)
 
 # Main
 if __name__ == '__main__':
-    knn()
+    # ---------- Load Train Data ----------
+    train_path = "D:/srtp/knn/testPy/data.csv"
+    data_train = pd.read_csv(train_path, usecols=["spotMean", "spotSTD", "speedMean", "speedSTD", "spotMax"])
+    label_train = pd.read_csv(train_path, usecols=["id"])
+
+    # ---------- Fit Knn Model ----------
+    knn = KNeighborsClassifier(n_neighbors=3)  # knn model name: knn
+    knn.fit(data_train, label_train)
+
+    ec_test = [3.629381443,	0.323199987, 3.042405728, 2.093326917, 4.278350515]
+    ec_predict = knn.predict(ec_test)
+    print(ec_predict)
--- processData.py
+++ processData.py
@@ -1,4 +1,7 @@
-import csv
+# function: include data process functions
+# variable: data input and output(label) path
+# update: 2024/5/14
+
 import pandas as pd
 import glob
 import os
@@ -6,31 +9,48 @@ import os
 
 def process_data(file_target):
     file_name = data_path + "*" + file_target + "*"
+    combined_temp = pd.DataFrame()
+
     for file in glob.glob(file_name):
+        # process data
         raw_data = pd.read_csv(file)
         raw_data["id"] = file_target
-        fil_new = file.replace("input", "label")
-        raw_data.to_csv(fil_new, index=False)
+
+        # save new data
+        file_new = file.replace("input", "label")
+        raw_data.to_csv(file_new, index=False)
         print(file)
 
+        # get combined csv
+        combined_temp = pd.concat([combined_temp, raw_data], ignore_index=True)
 
-def add_label(data, target):
-    data["id"] = target
-    return data
+    return combined_temp
 
 
-# data path
+# ---------- Data Path ----------
 # caution: notice function process_data() when changing data_path
 data_path = "D:/srtp/knn/testPy/input/"
 
-# output path
+# ---------- Output Path ----------
 # caution: notice function process_data() when changing data_path_output
 data_path_output = "D:/srtp/knn/testPy/label/"
 # check if output folder exists
 if not os.path.exists(data_path_output):
     os.makedirs(data_path_output)  # create output folder
 
-# keywords
-process_data("EC")
-process_data("KP")
-process_data("SA")
+# create combined csv
+combined_csv = pd.DataFrame()
+
+# ---------- Process Data ----------
+combined_temp = process_data("EC")
+combined_csv = pd.concat([combined_csv, combined_temp], ignore_index=True)
+
+combined_temp = process_data("KP")
+combined_csv = pd.concat([combined_csv, combined_temp], ignore_index=True)
+
+combined_temp = process_data("SA")
+combined_csv = pd.concat([combined_csv, combined_temp], ignore_index=True)
+
+# ---------- Save Combined Csv ----------
+combined_path = data_path.replace("input/", "") + "data.csv"
+combined_csv.to_csv(combined_path, index=False)
