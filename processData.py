60aba158e4b5f81e676eef4677a8bf47bc2a4d49
Yann <yanshen0117@outlook.com>
2024-05-14 20:22:27 +0800

    2024/5/14 20:22 master

--- processData.py
+++ processData.py
@@ -1,13 +1,36 @@
-import numpy as np
+import csv
+import pandas as pd
+import glob
+import os
 
 
-def calc_character_data(inData):
-    outData = inData
-    return outData
+def process_data(file_target):
+    file_name = data_path + "*" + file_target + "*"
+    for file in glob.glob(file_name):
+        raw_data = pd.read_csv(file)
+        raw_data["id"] = file_target
+        fil_new = file.replace("input", "label")
+        raw_data.to_csv(fil_new, index=False)
+        print(file)
 
 
-rawData = np.load()
+def add_label(data, target):
+    data["id"] = target
+    return data
 
-labelData = np.array()
 
-characterData = calc_character_data(rawData)
+# data path
+# caution: notice function process_data() when changing data_path
+data_path = "D:/srtp/knn/testPy/input/"
+
+# output path
+# caution! notice function process_data() when changing data_path_output
+data_path_output = "D:/srtp/knn/testPy/label/"
+# check if output folder exists
+if not os.path.exists(data_path_output):
+    os.makedirs(data_path_output)  # create output folder
+
+# keywords
+process_data("EC")
+process_data("KP")
+process_data("SA")
